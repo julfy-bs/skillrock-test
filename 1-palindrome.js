4092600f6222efddec06e02d04a84cf6aeb86559
julfy-bs <julfy.web@gmail.com>
2024-11-20 16:11:46 +0300

    feat: add palindrome algorithm jsdoc.

--- 1-palindrome.js
+++ 1-palindrome.js
@@ -1,4 +1,25 @@
+/**
+ * @param str { string } - Любая не пустая строка.
+ * @returns boolean - Ответ является ли строка палиндромом.
+ *
+ * @name Проверка на палиндром
+ * @description Напишите функцию, которая проверяет, является ли строка палиндромом. Палиндром
+ * — это слово, фраза, число или другая последовательность символов, которая
+ * читается одинаково слева направо и справа налево (игнорируя пробелы, знаки
+ * препинания и регистр).
+ *
+ * @example
+ * // returns true;
+ * isPalindrome('А роза упала на лапу Азора');
+ * @example
+ * // returns false;
+ * isPalindrome('Привет');
+ */
 function isPalindrome(str) {
+  if (typeof str !== 'string') {
+    return false;
+  }
+
   if (!str || str.length === 0) {
     return false;
   }
@@ -8,5 +29,5 @@ function isPalindrome(str) {
   return strWithoutPunctuation === strWithoutPunctuation.split('').reverse().join('');
 }
 
-console.log(isPalindrome('топот')); // true
-console.log(isPalindrome('привет')); // false
+console.log(isPalindrome('А роза упала на лапу Азора'));
+console.log(isPalindrome('Привет'));
