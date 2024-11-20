e136e3516b263f824d6e050732c574944d8b0ee6
julfy-bs <julfy.web@gmail.com>
2024-11-20 16:03:42 +0300

    feat: add fizzbuzz algorithm solution.

---
+++ 2-fizzBuzz.js
@@ -0,0 +1,30 @@
+// Напишите функцию, которая выводит числа от 1 до 100.
+// Но для кратных трём выводите "Fizz" вместо числа, а для кратных пяти — "Buzz".
+// Для чисел, кратных как трём, так и пяти, выводите "FizzBuzz".
+
+/**
+ * @param num{number}
+ */
+function fizzBuzz(num) {
+  if (typeof num !== 'number') {
+    console.log( `Аргумент должен быть числом`);
+  }
+
+  if (num < 1 || num > 100) {
+    console.log(`Аргумент должен быть в диапазоне между 1 и 100, сейчас передано число - ${num}`);
+  }
+
+  for (let i = num; i > 0; i--) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('FizzBuzz');
+    } else if (i % 3 === 0) {
+      console.log('Fizz');
+    } else if (i % 5 === 0) {
+      console.log('Buzz');
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+fizzBuzz(100);
