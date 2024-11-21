2e9fa707f158c7bb735b5899b15d459a47f4ff1e
julfy-bs <julfy.web@gmail.com>
2024-11-21 11:45:00 +0300

    feat: init react app.

---
+++ src/features/chunk-array/chunkArray.ts
@@ -0,0 +1,38 @@
+/**
+ * @param array { array } - Принимает не пустой массив.
+ * @param size { number } - Принимает натуральное число больше 1, определяющее
+ *   количество элементов в разбитых подмассивах.
+ * @returns array - возвращает массив с массивами в которых по {@link size}
+ *   элементов.
+ *
+ * @description Напишите функцию, которая разбивает массив на группы заданного
+ *   размера.
+ */
+
+function chunkArray<T>(array: T[], size: number): T[] {
+	if (array.length === 0) {
+		return array;
+	}
+	
+	if (size <= 1) {
+		return array;
+	}
+	
+	if (Math.floor(size) !== +size) {
+		throw SyntaxError(`параметр size - должен быть натуральным числом больше 1, сейчас size не натуральное число.`);
+	}
+	
+	let currentIndex = 0;
+	let resultArray: T[] = [];
+	array.forEach((_, index) => {
+		if (index === currentIndex) {
+			resultArray.push(array.slice(index, index + size) as T);
+			currentIndex += size;
+		}
+	});
+	
+	return resultArray;
+}
+
+console.log(chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+	, 2.1)); // [[1, 2], [3, 4], [5, 6],[7, 8]]
--- src/features/palindrome/palindrome.ts
+++ src/features/palindrome/palindrome.ts
@@ -3,7 +3,8 @@
  * @returns boolean - Ответ является ли строка палиндромом.
  *
  * @name Проверка на палиндром
- * @description Напишите функцию, которая проверяет, является ли строка палиндромом. Палиндром
+ * @description Напишите функцию, которая проверяет, является ли строка
+ *   палиндромом. Палиндром
  * — это слово, фраза, число или другая последовательность символов, которая
  * читается одинаково слева направо и справа налево (игнорируя пробелы, знаки
  * препинания и регистр).
@@ -15,18 +16,17 @@
  * // returns false;
  * isPalindrome('Привет');
  */
-function isPalindrome(str) {
-  if (typeof str !== 'string') {
-    return false;
-  }
-
-  if (!str || str.length === 0) {
-    return false;
-  }
-
-  const regex = /([~`!@#$%^&*(){}\[\];:"'<,.>?\/\\|\-_+=])|\s+/g;
-  const strWithoutPunctuation = str.toLowerCase().replace(regex, '')
-  return strWithoutPunctuation === strWithoutPunctuation.split('').reverse().join('');
+function isPalindrome(str: string) {
+	if (!str || str.length === 0) {
+		return false;
+	}
+	
+	const regex: RegExp = /([~`!@#$%^&*(){}\[\];:"'<,.>?\/\\|\-_+=])|\s+/g;
+	const strWithoutPunctuation: string = str.toLowerCase()
+		.replace(regex, '');
+	return strWithoutPunctuation === strWithoutPunctuation.split('')
+		.reverse()
+		.join('');
 }
 
 console.log(isPalindrome('А роза упала на лапу Азора'));
